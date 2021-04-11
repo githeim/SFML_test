acfd00dc333a7df2d0cee141d8842f5a7c002274
nowhere <nowhere@nowhere.com>
2021-04-11 12:24:04 +0900

    prerequisite script is modified

--- 00_SFML_prerequisite.sh
+++ 00_SFML_prerequisite.sh
@@ -1,2 +1,15 @@
 #!/bin/bash
+sudo apt-get install -y ninja-build
 sudo apt-get install libsfml-dev -y
+
+# google test installation
+sudo apt-get install libgtest-dev -y
+pushd /usr/src/gtest
+ls
+sudo cmake CMakeLists.txt
+sudo make -j12
+sudo cp lib/*.a /usr/lib
+sudo mkdir -p /usr/local/lib/gtest
+sudo ln -s /usr/lib/libgtest.a /usr/local/lib/gtest/libgtest.a
+sudo ln -s /usr/lib/libgtest_main.a /usr/local/lib/gtest/libgtest_main.a
+popd
